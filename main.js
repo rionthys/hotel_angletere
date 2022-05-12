98abe7bb7e5f337b83b9b5be25519b9d084074ce
rionthys <serdiudumitras@gmail.com>
2022-05-12 10:58:50 +0300

    practica la comp

--- main.js
+++ main.js
@@ -12,9 +12,18 @@ const swiper = new Swiper('.swiper', {
   });
 
  let header = document.querySelector('.header_glavnii');
+ let btn_broni = document.querySelector('.btn_broni');
+ let ticking = false;
+ let last_known_scroll_position = 0;
 
-//  document.addEventListener('DMMContentLoaded', () =>{
-//      window.onscroll = function(e){
-//          header.style.background = "#30080e";
-//      }
-//  })
+ document.addEventListener('scroll', function(e) {
+    if(window.scrollY > last_known_scroll_position){
+            header.style.background = "#30080e";
+            btn_broni.style.border = "2px solid rgb(255, 255, 255)";
+    }
+    else{
+        header.style.background = "linear-gradient(black, rgba(255, 0, 0, 0))";
+            btn_broni.style.border = "none";
+    }
+    last_known_scroll_position = window.scrollY;
+})
